1b2e8d9ee39bd516af0ee42b3f015f0077d7026c
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-01-31 00:54:55 -0500

    No longer fullscreen. Changed life.org to README.org
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- init.el
+++ init.el
@@ -12,3 +12,19 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/initfile.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Documents/SchoolStuff/PHYS2400/notes.org")))
+ '(package-selected-packages
+   (quote
+    (cdlatex auctex TeX AUCTeX LaTeX-mode LaTeX latex expand-region jedi flycheck outline-mode use-package undo-tree rainbow-delimiters org-bullets org magit htmlize diminish auto-complete which-key try outline-magic darkokai-theme counsel ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#242728" :foreground "#f8fbfc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
