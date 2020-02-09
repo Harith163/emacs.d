8fb3ba2d9719522660974a1ac00c2dbbaa613979
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-02-09 15:04:02 -0500

    Changed theme, added a powerline, and moved customize * to init.el.
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- init.el
+++ init.el
@@ -12,19 +12,25 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/initfile.org"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Documents/SchoolStuff/PHYS2400/notes.org")))
+ '(custom-safe-themes
+   (quote
+    ("d71f6c718dab453b625c407adc50479867a557668d5c21599a1ebea204d9e4f3" "2eb1f5551310e99101f0f9426485ab73aa5386054da877aacd15d438382bb72e" default)))
  '(package-selected-packages
    (quote
-    (cdlatex auctex TeX AUCTeX LaTeX-mode LaTeX latex expand-region jedi flycheck outline-mode use-package undo-tree rainbow-delimiters org-bullets org magit htmlize diminish auto-complete which-key try outline-magic darkokai-theme counsel ace-window))))
+    (powerline cdlatex auctex TeX AUCTeX LaTeX-mode LaTeX latex expand-region jedi flycheck outline-mode use-package undo-tree rainbow-delimiters org-bullets org magit htmlize diminish auto-complete which-key try outline-magic darkokai-theme counsel ace-window))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#242728" :foreground "#f8fbfc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :weight normal :height 158 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
+
+
