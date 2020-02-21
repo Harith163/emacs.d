0f509f6098f5fd3b334505b4a7813dbea8a4f93f
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-02-21 04:12:45 -0500

    New theme!!!

--- init.el
+++ init.el
@@ -18,10 +18,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("37ba833442e0c5155a46df21446cadbe623440ccb6bbd61382eb869a2b9e9bf9" "563404393f53f6a989199c09a5cb8f663267495ca32d969099456f7eb9d70493" "d71f6c718dab453b625c407adc50479867a557668d5c21599a1ebea204d9e4f3" "2eb1f5551310e99101f0f9426485ab73aa5386054da877aacd15d438382bb72e" default)))
+    ("c71d87d49a42b5c2220a648ffd9a45db115ab7d1b6406e83dae107fd4ccf53cb" default)))
  '(package-selected-packages
    (quote
-    (powerline-mode powerline cdlatex auctex TeX AUCTeX LaTeX-mode LaTeX latex expand-region jedi flycheck outline-mode use-package undo-tree rainbow-delimiters org-bullets org magit htmlize diminish auto-complete which-key try outline-magic darkokai-theme counsel ace-window))))
+    (smog seti-theme powerline-mode powerline cdlatex auctex TeX AUCTeX LaTeX-mode LaTeX latex expand-region jedi flycheck outline-mode use-package undo-tree rainbow-delimiters org-bullets org magit htmlize diminish auto-complete which-key try outline-magic darkokai-theme counsel ace-window))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
