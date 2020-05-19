332093dc07260f7c401e1fb1decf50e5d75faca6
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-05-19 00:20:50 -0400

    Things and stuff. PDF-tools!
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- init.el
+++ init.el
@@ -11,16 +11,22 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;;Use org-babel to parse the init file. Something about tangling?? Must look into it.
+(org-babel-load-file (expand-file-name "~/.emacs.d/initfile.org"))
+
+
+;; '(fringe-mode 0 nil (fringe))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(fringe-mode 0 nil (fringe))
  '(package-selected-packages
    (quote
-    (kurecolor pretty-symbols modus-vivendi-theme restart-emacs projectile rainbow-mode god-mode all-the-icons-ibuffer all-the-icons-ivy-rich ivy-rich all-the-icons-ivy company linum-relative pretty-mode autothemer dashboard powerline-mode powerline cdlatex auctex TeX AUCTeX LaTeX-mode LaTeX latex expand-region flycheck outline-mode use-package undo-tree rainbow-delimiters org-bullets org magit htmlize diminish auto-complete which-key try outline-magic darkokai-theme counsel ace-window))))
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/initfile.org"))
-
-
+    (pdf-tools which-key use-package undo-tree try restart-emacs rainbow-mode rainbow-delimiters projectile pretty-symbols pretty-mode powerline outline-magic org-bullets modus-vivendi-theme magit linum-relative kurecolor god-mode flycheck expand-region diminish dashboard darkokai-theme counsel company cdlatex autothemer auto-complete auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
