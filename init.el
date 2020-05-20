2a2a0e6bba85f0202ab0f1a5edd6ff7d78c758ac
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-05-19 22:28:56 -0400

    Blah
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- init.el
+++ init.el
@@ -16,17 +16,3 @@
 
 
 ;; '(fringe-mode 0 nil (fringe))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ibuffer-git ibuffer-tramp ibuffer-vc pdf-tools which-key use-package undo-tree try restart-emacs rainbow-mode rainbow-delimiters projectile pretty-symbols pretty-mode powerline outline-magic org-bullets modus-vivendi-theme magit linum-relative kurecolor god-mode flycheck expand-region diminish dashboard darkokai-theme counsel company cdlatex autothemer auto-complete auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- themes/trans-side-theme.el
+++ themes/trans-side-theme.el
@@ -1,90 +1,212 @@
-(deftheme trans-side
-  "Created 2020-02-21.")
+(require 'autothemer)
 
-(custom-theme-set-faces
- 'trans-side
- '(font-lock-builtin-face ((((class color) (min-colors 89)) (:foreground "#be2b95"))))
- '(font-lock-comment-face ((((class color) (min-colors 89)) (:foreground "#37465f"))))
- '(font-lock-negation-char-face ((((class color) (min-colors 89)) (:foreground "#6751a6"))))
- '(font-lock-constant-face ((((class color) (min-colors 89)) (:foreground "#6751a6"))))
- '(font-lock-doc-face ((((class color) (min-colors 89)) (:foreground "#bbadd0"))))
- '(font-lock-function-name-face ((((class color) (min-colors 89)) (:foreground "#913e88"))))
- '(font-lock-keyword-face ((((class color) (min-colors 89)) (:bold ((class color) (min-colors 89)) :foreground "#00ddf4"))))
- '(font-lock-string-face ((((class color) (min-colors 89)) (:foreground "#e85e8a"))))
- '(font-lock-type-face ((((class color) (min-colors 89)) (:foreground "#ff81b8"))))
- '(font-lock-variable-name-face ((((class color) (min-colors 89)) (:foreground "#00d3ff"))))
- '(font-lock-warning-face ((((class color) (min-colors 89)) (:foreground "#ff0069" :background "#0a0a0a"))))
- '(region ((((class color) (min-colors 89)) (:background "#f8f8f8" :foreground "#141414"))))
- '(highlight ((((class color) (min-colors 89)) (:foreground "#d0d0d0" :background "#3a3a3a"))))
- '(fringe ((((class color) (min-colors 89)) (:background "#272727" :foreground "#bcbcbc"))))
- '(cursor ((((class color) (min-colors 89)) (:background "#3a3a3a"))))
- '(isearch ((((class color) (min-colors 89)) (:bold t :foreground "#ff0000" :background "#3a3a3a"))))
- '(mode-line ((((class color) (min-colors 89)) (:box (:line-width 1 :color nil) :foreground "#e85e8a" :background "#141414" :weight normal))))
- '(mode-line-inactive ((((class color) (min-colors 89)) (:box (:line-width 1 :color nil :style pressed-button) :foreground "#e85e8a" :background "#141414" :weight normal))))
- '(mode-line-buffer-id ((((class color) (min-colors 89)) (:bold t :foreground "#e85e8a" :background nil))))
- '(mode-line-highlight ((((class color) (min-colors 89)) (:foreground "#00ddf4" :box nil :weight bold))))
- '(mode-line-emphasis ((((class color) (min-colors 89)) (:foreground "#f8f8f8"))))
- '(vertical-border ((((class color) (min-colors 89)) (:foreground "#d0d0d0"))))
- '(minibuffer-prompt ((((class color) (min-colors 89)) (:bold t :foreground "#00ddf4"))))
- '(link ((((class color) (min-colors 89)) (:foreground "#6751a6" :underline t))))
- '(org-code ((((class color) (min-colors 89)) (:foreground "#e4e4e4"))))
- '(org-hide ((((class color) (min-colors 89)) (:foreground "#bcbcbc"))))
- '(org-level-1 ((t (:foreground "#13c9f2" :weight bold :height 1.1))))
- '(org-level-2 ((t (:foreground "#e85e8a" :weight normal))))
- '(org-level-3 ((t (:foreground "#00ddf4" :weight bold))))
- '(org-level-4 ((t (:foreground "#6751a6" :weight normal))))
- '(org-date ((((class color) (min-colors 89)) (:underline t :foreground "#00d3ff"))))
- '(org-footnote ((((class color) (min-colors 89)) (:underline t :foreground "#bcbcbc"))))
- '(org-link ((((class color) (min-colors 89)) (:underline t :foreground "#ff81b8"))))
- '(org-special-keyword ((((class color) (min-colors 89)) (:foreground "#913e88"))))
- '(org-block ((((class color) (min-colors 89)) (:foreground "#d0d0d0"))))
- '(org-quote ((((class color) (min-colors 89)) (:inherit org-block :slant italic))))
- '(org-verse ((((class color) (min-colors 89)) (:inherit org-block :slant italic))))
- '(org-todo ((((class color) (min-colors 89)) (:box (:line-width 1 :color "#d0d0d0") :foreground "#00ddf4" :bold t))))
- '(org-done ((((class color) (min-colors 89)) (:box (:line-width 1 :color "#3a3a3a") :bold t :foreground "#4c4c4c"))))
- '(org-warning ((((class color) (min-colors 89)) (:underline t :foreground "#ff0069"))))
- '(org-agenda-structure ((((class color) (min-colors 89)) (:weight bold :foreground "#d0d0d0" :box (:color "#bcbcbc") :background "#3a3a3a"))))
- '(org-agenda-date ((((class color) (min-colors 89)) (:foreground "#00d3ff" :height 1.1))))
- '(org-agenda-date-weekend ((((class color) (min-colors 89)) (:weight normal :foreground "#bcbcbc"))))
- '(org-agenda-date-today ((((class color) (min-colors 89)) (:weight bold :foreground "#00ddf4" :height 1.4))))
- '(org-agenda-done ((((class color) (min-colors 89)) (:foreground "#4c4c4c"))))
- '(org-scheduled ((((class color) (min-colors 89)) (:foreground "#ff81b8"))))
- '(org-scheduled-today ((((class color) (min-colors 89)) (:foreground "#913e88" :weight bold :height 1.2))))
- '(org-ellipsis ((((class color) (min-colors 89)) (:foreground "#be2b95"))))
- '(org-verbatim ((((class color) (min-colors 89)) (:foreground "#bcbcbc"))))
- '(org-document-info-keyword ((((class color) (min-colors 89)) (:foreground "#913e88"))))
- '(font-latex-bold-face ((((class color) (min-colors 89)) (:foreground "#ff81b8"))))
- '(font-latex-italic-face ((((class color) (min-colors 89)) (:foreground "#00d3ff" :italic t))))
- '(font-latex-string-face ((((class color) (min-colors 89)) (:foreground "#e85e8a"))))
- '(org-sexp-date ((((class color) (min-colors 89)) (:foreground "#bcbcbc"))))
- '(ivy-current-match ((((class color) (min-colors 89)) (:foreground "#d0d0d0" :inherit highlight :underline t))))
- '(warning ((((class color) (min-colors 89)) (:foreground "#ff0069"))))
- '(ac-completion-face ((((class color) (min-colors 89)) (:underline t :foreground "#00ddf4"))))
- '(undo-tree-visualizer-current-face ((((class color) (min-colors 89)) :foreground "#be2b95")))
- '(undo-tree-visualizer-default-face ((((class color) (min-colors 89)) :foreground "#e4e4e4")))
- '(undo-tree-visualizer-unmodified-face ((((class color) (min-colors 89)) :foreground "#00d3ff")))
- '(undo-tree-visualizer-register-face ((((class color) (min-colors 89)) :foreground "#ff81b8")))
- '(trailing-whitespace ((((class color) (min-colors 89)) :foreground nil :background "#ff0000")))
- '(rainbow-delimiters-depth-1-face ((((class color) (min-colors 89)) :foreground "#f8f8f8")))
- '(rainbow-delimiters-depth-2-face ((((class color) (min-colors 89)) :foreground "#ff81b8")))
- '(rainbow-delimiters-depth-3-face ((((class color) (min-colors 89)) :foreground "#00d3ff")))
- '(rainbow-delimiters-depth-4-face ((((class color) (min-colors 89)) :foreground "#6751a6")))
- '(rainbow-delimiters-depth-5-face ((((class color) (min-colors 89)) :foreground "#00ddf4")))
- '(rainbow-delimiters-depth-6-face ((((class color) (min-colors 89)) :foreground "#f8f8f8")))
- '(rainbow-delimiters-depth-7-face ((((class color) (min-colors 89)) :foreground "#ff81b8")))
- '(rainbow-delimiters-depth-8-face ((((class color) (min-colors 89)) :foreground "#00d3ff")))
- '(magit-section-heading ((((class color) (min-colors 89)) (:foreground "#00ddf4" :weight bold))))
- '(magit-section-highlight ((((class color) (min-colors 89)) (:background "#272727"))))
- '(magit-diff-context-highlight ((((class color) (min-colors 89)) (:background "#3a3a3a" :foreground "#d0d0d0"))))
- '(magit-diffstat-added ((((class color) (min-colors 89)) (:foreground "#ff81b8"))))
- '(magit-diffstat-removed ((((class color) (min-colors 89)) (:foreground "#00d3ff"))))
- '(magit-process-ok ((((class color) (min-colors 89)) (:foreground "#913e88" :weight bold))))
- '(magit-process-ng ((((class color) (min-colors 89)) (:foreground "#ff0000" :weight bold))))
- '(magit-log-author ((((class color) (min-colors 89)) (:foreground "#d0d0d0"))))
- '(magit-hash ((((class color) (min-colors 89)) (:foreground "#e4e4e4"))))
- '(lazy-highlight ((((class color) (min-colors 89)) (:foreground "#e4e4e4" :background "#3a3a3a"))))
- '(rainbow-delimiters-unmatched-face ((((class color) (min-colors 89)) :foreground "#ff0000")))
- '(default ((((class color) (min-colors 89)) (:background "#080808" :foreground "#f8f8f8"))))
-)
+(unless (>= emacs-major-version 24)
+  (error "Requires Emacs 24 or later"))
+
+
+(autothemer-deftheme trans-side "Custom theme inspired by the colors of the Trans Flag."
+ 
+		     ;; Specify the color classes used by the theme
+		     ((((class color) (min-colors #xFFFFFF)))
+		      
+		      ;; Specify the color palette for each of the classes above.
+		      ;;basic bg/fg colors
+		      (bg-main "#00000f")
+		      (bg-black "#080808")
+		      (bg-dark "#303030")
+		      (bg-gray "#c6c6c6")
+		      (fg-white "#FFFFFF")
+		      (fg-dark "#5e5e5e")
+		      (fg-gray "#919191")
+
+		      ;;darks
+		      (pink-dark "#a43261")
+		      (blue-dark "#006ca5")
+		      (aqua-dark "#007086")
+		      (purple-dark "#6751a6")
+		      (magenta-dark "#913e88")
+		      (blue2-dark "#0061b1")
+
+		      ;;lights
+		      (pink-light "#ff81b8")
+		      (blue-light "#3bd6ff")
+		      (aqua-light "#00ddf4")
+		      (purple-light "#a38cc5")
+		      (magenta-light "#ffa7f6")
+		      (blue2-light "#93c9ff")
+
+		      ;;light alt
+		      (pink-light-alt "#e85e8a")
+		      (aqua-light-alt "#00d3ff")
+
+		      ;;Alt grays and darks
+		      (mygray1 "#f8f8f8")
+		      (mygray1-dark "#d0d0d0")
+		      (mygray2 "#3a3a3a")
+		      (mygray2-dark "#141414")
+
+		      ;;Level Colors
+		      (level1 "#FF619D")
+		      (level2 "#00ccFF")
+		      (level3 "#8368D5")
+		      (level4 "#00eeff")
+		      (level5 "#F561FF")
+		      (level6 "#00FCEE")
+		      (level7 "#6B62E2")
+		      (level8 "#00FF75")
+
+		      ;;Alternate Pallate
+		      (pastel-blue "#7eb8da")
+		      (pastel-aqua "#92ddea")
+		      (pastel-pink "#ffa5d8")
+		      (pastel-lilac "#be9ddf")
+		      (pastel-violet "#9579d1")
+
+		      (standout "#00FF94")
+				      
+		      ;;Warning, comment, and other miscellanious colors
+		      (builtin "#8C73ED")
+		      (comment "#37465f")
+		      (warning "#ff0069"))
+		     
+		     ;; specifications for Emacs faces.
+		     
+		     (;; Default faces. Most commonly seen, used and inherited.
+		      (default (:font "DejaVu Sans Mono" :foreground fg-white :background bg-main :height 150))
+		      (font-lock-builtin-face (:foreground builtin))
+		      (font-lock-comment-face (:foreground comment))
+		      (font-lock-doc-face (:foreground purple-dark))
+		      (font-lock-keyword-face (:foreground pastel-lilac :bold t))
+		      (font-lock-negation-char-face (:foreground purple-dark))
+		      (font-lock-string-face (:foreground pink-light-alt))
+		      (font-lock-type-face (:foreground pink-light))
+		      
+		      (font-lock-function-name-face (:foreground pastel-blue))
+		      (font-lock-variable-name-face (:foreground aqua-light-alt))
+		      (font-lock-constant-face (:foreground pastel-violet))
+		      
+		      (font-lock-warning-face (:foreground warning))
+
+		      ;;Other basic things
+		      (scroll-bar nil)
+		      (border nil)
+		      (cursor (:background fg-gray))
+		      (fringe (:background bg-main))
+		      (highlight (:foreground mygray1-dark :background mygray2))
+		      (region (:foreground mygray2-dark :background mygray1))
+
+		      ;;more basic things!!
+		      (bold (:weight 'bold))
+		      (warning (:weight 'bold :foreground warning))
+
+		      ;;line number mode
+		      (linum (:height 0.85 :inherit ('shadow 'default)))
+		      (linum-relative-current-face (:weight 'bold :foreground pastel-aqua :background mygray2 :inherit 'linum))
+		      (line-number (:inherit ('shadow 'default)))
+		      (line-number-current-line (:inherit 'line-number))
+
+		      ;;Mode line 
+		      (mode-line (:box (:line-width 1 :color nil) :foreground aqua-light-alt :background bg-black))
+		      (mode-line-inactive (:weight 'light :box (:line-width 1 :color nil :style 'pressed-button) :foreground aqua-light-alt :background bg-black :inherit 'mode-line))
+		      (mode-line-highlight (:box nil :foreground aqua-light :weight 'bold))
+		      (mode-line-emphasis (:weight 'bold :foreground mygray1))
+		      (mode-line-buffer-id (:weight 'bold :bold t :foreground aqua-light-alt :background nil))
+
+		      ;;iSearch
+		      (isearch (:weight 'bold :foreground warning :background mygray2))
+		      (isearch-fail (:background bg-black))
+
+		      ;;Minibuffer prompt
+		      (minibuffer-prompt (:weight 'bold :foreground aqua-light))
+
+		      ;;Org-mode stuff
+		      (org-code (:foreground mygray1))
+		      (org-hide (:foreground mygray1-dark))
+		      (org-date (:foreground aqua-light-alt :underline t))
+		      (org-footnote (:foreground mygray1-dark :underline t))
+		      (org-link (:foreground pink-light :underline t))
+		      (org-special-keyword (:foreground magenta-dark))
+		      (org-quote (:inherit 'org-block :slant 'italic))
+		      (org-verse (:inherit 'org-block :slant 'italic))
+		      (org-todo (:foreground aqua-light :bold t :box nil))
+		      (org-done (:foreground pink-dark :bold t :box nil))
+		      (org-warning (:foreground warning :underline t))
+		      (org-agenda-structure (:foreground mygray1-dark :background bg-dark :weight 'bold :box (:color bg-gray) ))
+		      (org-agenda-date (:foreground aqua-light-alt :height 1.1))
+		      (org-agenda-date-weekend (:foreground bg-gray :weight 'normal ))
+		      (org-agenda-date-today (:foreground aqua-light :weight 'bold :height 1.4))
+		      (org-agenda-structure (:foreground aqua-light-alt :height 1.1))
+		      (org-agenda-done (:foreground mygray2 :height 1.1))
+		      (org-scheduled (:foreground pink-light))
+		      (org-scheduled-today (:foreground magenta-dark))
+		      (org-ellipsis (:foreground builtin))
+		      (org-varbatim (:foreground bg-gray))
+		      (org-document-info-keyword (:foreground pastel-lilac :height 1.1))
+		      (org-document-info (:foreground pastel-blue :height 1.1))
+		      (org-document-title (:foreground pastel-blue :weight 'bold :height 1.1))
+
+		      (org-level-1 (:inherit 'outline-1 :weight 'bold :height 1.3 :slant 'italic))
+		      (org-level-2 (:inherit 'outline-2 :weight 'normal :height 1.1 :slant 'italic))
+		      (org-level-3 (:inherit 'outline-3 :weight 'normal))
+		      (org-level-4 (:inherit 'outline-4 :weight 'normal))
+		      (org-level-5 (:inherit 'outline-5 :weight 'normal))
+		      (org-level-6 (:inherit 'outline-6 :weight 'normal))
+		      (org-level-7 (:inherit 'outline-7 :weight 'normal))
+		      (org-level-8 (:inherit 'outline-8 :weight 'normal))
+
+		      (org-block (:background "#040014"))
+		      (org-block-begin-line (:inherit 'font-lock-comment-face :background "#140024"))
+		      (org-block-end-line (:inherit 'org-block-begin-line))
+
+		      ;;Rainbow delimiters
+		      (rainbow-delimiters-depth-1-face (:foreground mygray1))
+		      (rainbow-delimiters-depth-2-face (:foreground pink-light))
+		      (rainbow-delimiters-depth-3-face (:foreground aqua-light-alt))
+		      (rainbow-delimiters-depth-4-face (:foreground purple-dark))
+		      (rainbow-delimiters-depth-5-face (:foreground aqua-light))
+		      (rainbow-delimiters-depth-6-face (:foreground mygray1))
+		      (rainbow-delimiters-depth-7-face (:foreground pink-light))
+		      (rainbow-delimiters-depth-8-face (:foreground aqua-light-alt))
+		      (rainbow-delimiters-depth-9-face (:foreground purple-light :inherit 'rainbow-delimiters-base-face))
+		      (rainbow-delimiters-mismatched-face (:inherit 'rainbow-delimiters-unmatched-face))
+		      (rainbow-delimiters-unmatched-face (:foreground warning))
+		      (rainbow-delimiters-base-error-face (:foreground bg-black :inherit 'rainbow-delimiters-base-face))
+		      (rainbow-delimiters-base-face nil)
+
+		      ;;Outlines/Headings
+		      (outline-1 (:foreground level1))
+		      (outline-2 (:foreground level2))
+		      (outline-3 (:foreground level3))
+		      (outline-4 (:foreground level4))
+		      (outline-5 (:foreground level5))
+		      (outline-6 (:foreground level6))
+		      (outline-7 (:foreground level7))
+		      (outline-8 (:foreground level8))
+		      
+		      ;;Ace Window Mode stuff
+		      (aw-leading-char-face (:height 2.0 :inherit 'ace-jump-face-foreground))
+
+		      ;;latex faces
+		      (font-latex-sectioning-0-face (:height 1.1 :inherit 'font-latex-sectioning-1-face))
+		      (font-latex-sectioning-1-face (:height 1.1 :inherit 'font-latex-sectioning-2-face))
+		      (font-latex-sectioning-2-face (:height 1.1 :inherit 'font-latex-sectioning-3-face))
+		      (font-latex-sectioning-3-face (:height 1.1 :inherit 'font-latex-sectioning-4-face))
+		      (font-latex-sectioning-4-face (:height 1.1 :inherit 'font-latex-sectioning-5-face))
+		      (font-latex-sectioning-5-face (:weight 'bold :slant 'italic :foreground pastel-blue))
+
+		      (font-latex-math-face (:foreground standout :weight 'semi-bold))
+
+		      ;;Ivy and counsel stuff
+		      		      
+		      )	     
+		     ;; Forms after the face specifications are evaluated.
+		     ;; (palette vars can be used, read below for details.)
+		     )
+
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
 
 (provide-theme 'trans-side)
