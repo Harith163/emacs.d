d5bd98489ecbcc5063a4d7da96dfa9acf62eea1e
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-05-23 22:19:07 -0400

    More stuff and Things
    Labeled the functions and variable appropriately. Moved some stuff
    around. Added org-superstar and made the bullets a list. Also excluded
    autoloads from recentf.
    
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(org-agenda-files (quote ("~/Documents/life/README.org")))
  '(package-selected-packages
    (quote
-    (engine-mode dired-filter md4rd ivy-posframe company-fuzzy posframe vterm edit-server ibuffer-project ibuffer-projectile fancy-battery which-key use-package undo-tree try restart-emacs rainbow-mode rainbow-delimiters projectile pretty-symbols pretty-mode powerline pdf-tools outline-magic org-bullets modus-vivendi-theme magit linum-relative kurecolor ibuffer-vc ibuffer-tramp ibuffer-git god-mode flycheck expand-region diminish dashboard darkokai-theme counsel company cdlatex autothemer auto-complete auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer ace-window))))
+    (all-the-icons-dired org-superstar engine-mode dired-filter md4rd ivy-posframe company-fuzzy posframe vterm edit-server ibuffer-project ibuffer-projectile fancy-battery which-key use-package undo-tree try restart-emacs rainbow-mode rainbow-delimiters projectile pretty-symbols pretty-mode powerline pdf-tools outline-magic org-bullets modus-vivendi-theme magit linum-relative kurecolor ibuffer-vc ibuffer-tramp ibuffer-git god-mode flycheck expand-region diminish dashboard darkokai-theme counsel company cdlatex autothemer auto-complete auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
