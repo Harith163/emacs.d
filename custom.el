b2653c33857adf8d49ec89b1e084711a2cd0ec55
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-06-19 09:16:16 +0100

    ADDED EAF!!!
    Signed-off-by: kaushik.harith@gmail.com <kaushik.harith@gmail.com>

--- custom.el
+++ custom.el
@@ -4,13 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   (quote
-    ("~/Documents/life/README.org" "~/Documents/life/Ideas.org" "~/Documents/life/SchoolWork.org")))
+   '("~/Documents/life/README.org" "~/Documents/life/Ideas.org" "~/Documents/life/SchoolWork.org" "~/Documents/life/Habit.org"))
  '(package-selected-packages
-   (quote
-    (yaml-mode yaml w3 evil-org evil company-auctex which-key vterm use-package undo-tree try smartparens restart-emacs rainbow-mode rainbow-delimiters pretty-symbols pretty-mode powerline pdf-tools outline-magic org-superstar org-msg org-bullets mu4e-alert magit lorem-ipsum linum-relative kurecolor ivy-prescient ivy-posframe ibuffer-vc ibuffer-tramp ibuffer-projectile ibuffer-project ibuffer-git flycheck fancy-battery expand-region engine-mode edit-server dired-filter diminish dashboard darkokai-theme counsel company-fuzzy cdlatex autothemer auto-complete auctex all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-dired ace-window)))
- '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 25))
+   '(hide-mode-line unicode-fonts multiple-cursors yaml-mode which-key vterm use-package try smartparens restart-emacs rainbow-mode rainbow-delimiters pretty-symbols pretty-mode powerline pdf-tools outline-magic org-superstar org-msg org-gcal org-bullets mu4e-alert magit lorem-ipsum linum-relative kurecolor ivy-prescient ivy-posframe ibuffer-vc ibuffer-tramp ibuffer-projectile ibuffer-project ibuffer-git flycheck fish-completion fancy-battery expand-region evil-org engine-mode edit-server dired-filter diminish dashboard darkokai-theme counsel company-fuzzy company-auctex cdlatex autothemer auto-complete all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
