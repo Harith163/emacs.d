fa31359293198bb4297b64d88cfef8580daaddcf
kaushik.harith@gmail.com <kaushik.harith@gmail.com>
2020-08-03 11:39:25 +0530

    emacs

--- custom.el
+++ custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0dc7a24738282c5e9091faea1222ac90cab8991bc17979180d83a2dc9f721c5f" "b8e33b8025f11c2e41e9a0a9377bf916d27d49d17482ef0fdacd86978d37ae8e" default))
+ '(eaf-find-alternate-file-in-dired nil t)
  '(org-agenda-files
-   '("~/Documents/life/README.org" "~/Documents/life/Ideas.org" "~/Documents/life/SchoolWork.org" "~/Documents/life/Habit.org"))
+   '("~/Documents/life/README.org" "~/Documents/life/Ideas.org" "~/Documents/life/SchoolWork.org"))
  '(package-selected-packages
-   '(hide-mode-line unicode-fonts multiple-cursors yaml-mode which-key vterm use-package try smartparens restart-emacs rainbow-mode rainbow-delimiters pretty-symbols pretty-mode powerline pdf-tools outline-magic org-superstar org-msg org-gcal org-bullets mu4e-alert magit lorem-ipsum linum-relative kurecolor ivy-prescient ivy-posframe ibuffer-vc ibuffer-tramp ibuffer-projectile ibuffer-project ibuffer-git flycheck fish-completion fancy-battery expand-region evil-org engine-mode edit-server dired-filter diminish dashboard darkokai-theme counsel company-fuzzy company-auctex cdlatex autothemer auto-complete all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-dired ace-window)))
+   '(org-alert company-auctex auctex exwm eaf centaur-tabs hide-mode-line unicode-fonts multiple-cursors yaml-mode which-key vterm use-package try smartparens restart-emacs rainbow-mode rainbow-delimiters pretty-symbols pretty-mode powerline pdf-tools outline-magic org-superstar org-msg org-gcal org-bullets mu4e-alert magit lorem-ipsum linum-relative kurecolor ivy-prescient ivy-posframe ibuffer-vc ibuffer-tramp ibuffer-projectile ibuffer-project ibuffer-git flycheck fish-completion fancy-battery expand-region evil-org engine-mode edit-server dired-filter diminish dashboard darkokai-theme counsel company-fuzzy cdlatex autothemer auto-complete all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
