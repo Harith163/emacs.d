3e78e69d24c98349aeb6fcd1c10896f1556e4e71
60790 <kaushik@arch6002lt249.archeangroup.dc>
2023-07-31 13:05:37 +0530

    Changed some stuff
    * custom.el:

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("618ba1ad8fe0a597a950af503b37eca2b3154a90b170af67f8bc1eb5dd031faa" "08738c69fab04191d12067c68baa5114e01722db1444efb9e124951b63e0ef50" "fcc9d082922128eeccc7c0516248692733de810d6806be5ea25e9b000afeec26" "78256ff8c6362ed006aa11a5ae7c5a8d1ad505ba0274fed7cc0661dab1b5827e" default))
+   '("1e3243439aa654f07302277b43fb741619903a7b67993c0fb76fcb1db7b42924" "618ba1ad8fe0a597a950af503b37eca2b3154a90b170af67f8bc1eb5dd031faa" "08738c69fab04191d12067c68baa5114e01722db1444efb9e124951b63e0ef50" "fcc9d082922128eeccc7c0516248692733de810d6806be5ea25e9b000afeec26" "78256ff8c6362ed006aa11a5ae7c5a8d1ad505ba0274fed7cc0661dab1b5827e" default))
  '(warning-suppress-log-types '((auto-save)))
  '(warning-suppress-types '((auto-save) (auto-save))))
 (custom-set-faces
