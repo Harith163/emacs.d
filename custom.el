0f2f5072fdf20131186729a17eb207fa531eba83
60790 <kaushik@arch6002lt249.archeangroup.dc>
2023-11-02 13:34:51 +0530

    Last straight commit

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1e3243439aa654f07302277b43fb741619903a7b67993c0fb76fcb1db7b42924" "618ba1ad8fe0a597a950af503b37eca2b3154a90b170af67f8bc1eb5dd031faa" "08738c69fab04191d12067c68baa5114e01722db1444efb9e124951b63e0ef50" "fcc9d082922128eeccc7c0516248692733de810d6806be5ea25e9b000afeec26" "78256ff8c6362ed006aa11a5ae7c5a8d1ad505ba0274fed7cc0661dab1b5827e" default))
+   '("6ca663019600e8e5233bf501c014aa0ec96f94da44124ca7b06d3cf32d6c5e06" "4320a92406c5015e8cba1e581a88f058765f7400cf5d885a3aa9b7b9fc448fa7" "1e3243439aa654f07302277b43fb741619903a7b67993c0fb76fcb1db7b42924" "618ba1ad8fe0a597a950af503b37eca2b3154a90b170af67f8bc1eb5dd031faa" "08738c69fab04191d12067c68baa5114e01722db1444efb9e124951b63e0ef50" "fcc9d082922128eeccc7c0516248692733de810d6806be5ea25e9b000afeec26" "78256ff8c6362ed006aa11a5ae7c5a8d1ad505ba0274fed7cc0661dab1b5827e" default))
+ '(org-agenda-files
+   '("~/Documents/Project Altilium/Proposals/Proposal to commercialize ZnBr2 Flow Battery Technologies.org" "/home/kaushik/Dropbox/Org/Daily/Task List.org"))
  '(warning-suppress-log-types '((auto-save)))
  '(warning-suppress-types '((auto-save) (auto-save))))
 (custom-set-faces
