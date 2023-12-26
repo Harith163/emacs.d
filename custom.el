a9030decdd708b756c77b03a738ec6cb25797e76
Kaushik Skye Harith <kaushik.harith@gmail.com>
2023-12-26 14:43:35 +0530

    Stuff

--- custom.el
+++ custom.el
@@ -3,11 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(eaf-browser eaf spacious-padding all-the-icons-dired all-the-icons-completion all-the-icons-completions which-key vertico vc-use-package undo-tree restart-emacs rainbow-mode rainbow-delimiters pdf-tools org-modern orderless nano marginalia magit expand-region corfu consult cdlatex all-the-icons.el all-the-icons))
  '(package-vc-selected-packages
-   '((eaf :vc-backend Git :url "https://github.com/emacs-eaf/emacs-application-framework")
-     (vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
